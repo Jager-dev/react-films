9343ace555dccfb8e1898c62039aea2fd3fcb6f2
Ulan Zhakypov <zhakypovu@gmail.com>
2021-09-09 14:11:50 +0300

    exact last commit

--- src/views/FilmDetails/index.js
+++ src/views/FilmDetails/index.js
@@ -34,77 +34,84 @@ const FilmDetails = () => {
         setActors(data.cast)
         setActorLoading(false)
       })
-  },[])
+  }, [])
 
-  if (isLoading || actorLoading){
+  if (isLoading || actorLoading) {
     return (
-      <Spinner />
+      <Spinner/>
     )
   }
   return (
-    <div className={"row"}>
-      <div>
-        <button className="back" onClick={() => history.goBack()}>Back</button>
-      </div>
-      <div className="col-md-6">
-        <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${film.poster_path}`} alt={film.title} width={"400"}/>
-      </div>
-      <div className="col-md-6">
-        <h3>Name: {film.title}</h3>
-        <p>Overview: {film.overview}</p>
-        <p>Language: {film.original_language}</p>
-        <p>Rating: {film.vote_average}</p>
-        <p>Budget: {film.budget.toLocaleString()}$</p>
-        <p>Revenue: {film.revenue?.toLocaleString()}$</p>
-        <p>Release date: {film.release_date}</p>
-        <h5>Producers:</h5>
-        {
-          film.production_companies.map(company =>
-            <div key={company.id}>{company.name}</div>
-          )
-        }
+    <div style={{backgroundImage: `url(https://www.themoviedb.org/t/p/original${film.backdrop_path})`}}
+         className='bg'>
+      <div className={"row"}>
         <div>
-          <p><span>Genre:  </span>{film.genres?.map(item => <div
-            key={item}>{item.name}</div>)}
-
-          </p>
-          <p>
-            <span>Film duration: </span> {Math.floor(film.runtime / 60)}h {Math.floor(film.runtime % 60)}m
-          </p>
+          <button className="back" onClick={() => history.goBack()}>Back</button>
         </div>
-        {
-          film.production_countries.map(item => item).length > 1 ? <h5>Countries:</h5> : film.production_countries.length === 0 ? "" : <h5>Country:</h5>
-        }
-        {
-          film.production_countries.map(country =>
-            <div key={country.id}>{country.name}</div>
-          )
-        }
-        <h5 className={"mt-5"}>Actors:</h5>
-        <OwlCarousel className='owl-theme mt-3' loop margin={10} dots={false} items={4} nav>
+        <div className="col-md-6">
+          <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${film.poster_path}`} alt={film.title}
+               width={"400"}/>
+        </div>
+        <div className="col-md-6">
+          <h3>Name: {film.title}</h3>
+          <p>Overview: {film.overview}</p>
+          <p>Language: {film.original_language}</p>
+          <p>Rating: {film.vote_average}</p>
+          <p>Budget: {film.budget.toLocaleString()}$</p>
+          <p>Revenue: {film.revenue?.toLocaleString()}$</p>
+          <p>Release date: {film.release_date}</p>
+          <h5>Producers:</h5>
           {
-            actors.slice(actors, 10).map(actor =>
-              <div>
-                <Link to={`/actorsdetails/${actor.id}`}>
-                  {
-                    actor.profile_path === null ?
-                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBjZn8mOw7F4rtWWKbEIIHOr_w_GAeHiXPgA&usqp=CAU" height={"193.5"} alt=""/>
-                      :
-                      <img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt=""/>
-                  }
-                </Link>
-                <div>{actor.name}</div>
-              </div>
+            film.production_companies.map(company =>
+              <div key={company.id}>{company.name}</div>
             )
           }
-          <Link to={`/actors/${params.id}`}>View more --></Link>
-        </OwlCarousel>
-      </div>
-      {
+          <div>
+            <p><span>Genre:  </span>{film.genres?.map(item => <div
+              key={item}>{item.name}</div>)}
+
+            </p>
+            <p>
+              <span>Film duration: </span> {Math.floor(film.runtime / 60)}h {Math.floor(film.runtime % 60)}m
+            </p>
+          </div>
+          {
+            film.production_countries.map(item => item).length > 1 ?
+              <h5>Countries:</h5> : film.production_countries.length === 0 ? "" : <h5>Country:</h5>
+          }
+          {
+            film.production_countries.map(country =>
+              <div key={country.id}>{country.name}</div>
+            )
+          }
+          <h5 className={"mt-5"}>Actors:</h5>
+          <OwlCarousel className='owl-theme mt-3' loop margin={10} dots={false} items={4} nav>
+            {
+              actors.slice(actors, 10).map(actor =>
+                <div>
+                  <Link to={`/actorsdetails/${actor.id}`}>
+                    {
+                      actor.profile_path === null ?
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBjZn8mOw7F4rtWWKbEIIHOr_w_GAeHiXPgA&usqp=CAU"
+                          height={"193.5"} alt=""/>
+                        :
+                        <img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt=""/>
+                    }
+                  </Link>
+                  <div>{actor.name}</div>
+                </div>
+              )
+            }
+            <Link to={`/actors/${params.id}`}>View more --></Link>
+          </OwlCarousel>
+        </div>
+        {
           trailers.map(item =>
             <Fancy key={item.key} id={item.key}/>
           )
-      }
+        }
+      </div>
     </div>
   )
 }
