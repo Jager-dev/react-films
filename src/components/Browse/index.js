6410d2f6af434d218017a005c31773258a05c8d1
Ulan Zhakypov <zhakypovu@gmail.com>
2021-09-09 14:21:10 +0300

    edit

--- src/components/Browse/index.js
+++ src/components/Browse/index.js
@@ -3,7 +3,6 @@ import {Link, useHistory, useParams} from "react-router-dom";
 
 const Browse = () => {
   const [inputValue, setInputValue] = useState("")
-  const [isOpen, setIsOpen] = useState(true)
   const history = useHistory()
 
   const params = useParams()
@@ -14,9 +13,6 @@ const Browse = () => {
   const handleClick = () => {
     history.push(`/browse/${inputValue}`)
     setTimeout(() => setInputValue(""), 1000)
-  }
-  const handleClear = () => {
-    setIsOpen(false)
   }
   const enterPress = (e) => {
     if (e.key === "Enter"){
@@ -38,9 +34,6 @@ const Browse = () => {
           <input type="text" className={"browse__input"} placeholder={"Search a film, serial, actor..."} onKeyDown={enterPress} value={inputValue} onChange={input}/>
           <button className={"d-flex justify-content-center align-items-center"} onClick={handleClick} disabled={!inputValue.trim()}>
             search
-          </button>
-          <button className={"d-flex justify-content-flex-end align-items-center"} onClick={handleClear} disabled={!inputValue.trim()}>
-            clear
           </button>
         </div>
       </header>
